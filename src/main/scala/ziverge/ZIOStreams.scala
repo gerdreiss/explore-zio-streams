1c4d2a2efbc0fa9a0a64c893ffd23b41c50cacae
Gerd Reiss <gerdreiss@@users.noreply.github.com>
2022-08-31 07:40:56 +0200

    [Ziverge] ZIO Streams

--- src/main/scala/ziverge/ZIOStreams.scala
+++ src/main/scala/ziverge/ZIOStreams.scala
@@ -6,9 +6,28 @@ import java.io.IOException
 
 object ZIOStreams extends ZIOAppDefault:
 
-  val runStream =
-    ZStream.repeatZIO(Random.nextIntBetween(3, 100)).take(100) >>>
-      ZPipeline.filter[Int](_ % 3 == 0) >>>
-      ZSink.collectAll
+  val randomNumbers = ZStream.repeatZIO(Random.nextIntBetween(3, 100))
 
-  override def run = runStream.debug("Got: ")
+  val filterer = ZPipeline.filter[Int](_ % 3 == 0)
+  val doubler  = ZPipeline.map[Int, Int](_ * 2)
+
+  def collector(n: Int) = ZSink.collectAllN[Int](n)
+
+  val runStream = randomNumbers >>> filterer >>> doubler >>> collector(10)
+
+  val runStreams = randomNumbers
+    .broadcast(2, 16)
+    .flatMap { streams =>
+      val subscriber1 = streams(0) >>> doubler >>> filterer >>> collector(10)
+      val subscriber2 = streams(1) >>> doubler >>> filterer >>> collector(5)
+      subscriber1 <&> subscriber2
+    }
+
+  val runDynamicStreams =
+    for
+      shared <- randomNumbers.broadcastDynamic(16)
+      c1     <- shared >>> doubler >>> filterer >>> collector(10)
+      c2     <- shared >>> doubler >>> filterer >>> collector(5)
+    yield (c1, c2)
+
+  override def run = runDynamicStreams.debug("Got: ")
