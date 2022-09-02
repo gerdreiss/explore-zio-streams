68d7c0123727716606a255461aa26c550d5925f1
Gerd Reiss <gerdreiss@@users.noreply.github.com>
2022-09-02 19:07:08 +0200

    [ZIO Streams] Part 2

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # Exploring ZIO Streams with Rock the JVM and Ziverge!
 
 - [Rock The JVM - Part 1](https://youtu.be/bp3nM6bfzJk)
+- [Rock The JVM - Part 2](https://youtu.be/eewZHxuBA3E)
 
 - [Ziverge - Part 1](https://youtu.be/8hG_UY0Dazw)
--- src/main/scala/rockthejvm/ZIOStreams.scala
+++ src/main/scala/rockthejvm/ZIOStreams.scala
@@ -3,6 +3,8 @@ package rockthejvm
 import zio.*
 import zio.json.*
 import zio.stream.*
+import java.io.InputStream
+import java.io.IOException
 
 object ZIOStreams extends ZIOAppDefault:
 
@@ -45,4 +47,52 @@ object ZIOStreams extends ZIOAppDefault:
 
   val zio4: ZIO[Any, Nothing, Int] = strings.via(appLogic).run(sum)
 
-  override def run: UIO[Unit] = zio4.debug.unit
+  /** 
+   * Failures/Errors
+   */
+
+  val failStream: ZStream[Any, String, Int] =
+    ZStream.fromIterable(1 to 10) ++ ZStream.fail("boom") ++ ZStream.fromIterable(11 to 100)
+
+  class FakeInputStream[T <: Throwable](failAt: Int, failWith: => T) extends InputStream:
+    private val data: Array[Byte] = util.Random.nextString(8192).getBytes
+    private var index             = 0
+
+    override def read(): Int =
+      if index == failAt then throw failWith
+      else
+        data
+          .lift(index)
+          .fold(-1) { byte =>
+            index += 1
+            byte
+          }
+
+  val failingStream: ZStream[Any, IOException, String] =
+    ZStream
+      .fromInputStream(new FakeInputStream(9, new IOException("boom")), chunkSize = 1)
+      .map(_.toChar.toString)
+
+  val sink: ZSink[Any, Nothing, String, Nothing, String] =
+    ZSink.collectAll[String].map(_.mkString("-"))
+
+  val defectStream: ZStream[Any, Nothing, Int] =
+    ZStream.fromIterable(1 to 10) ++ ZStream.dieMessage("boom") ++ ZStream.fromIterable(11 to 100)
+
+  // recovery
+  val recoveryStream: ZStream[Any, Throwable, String] =
+    ZStream("A", "B", "C", "D", "E", "F", "G", "H", "I")
+
+  val recoveredStream = failingStream <> recoveryStream >>> sink
+
+  // catch
+  val caughtStream = failingStream.catchAll(_ => recoveryStream) >>> sink
+
+  val caughtSomeStream = failingStream
+    .catchSome { case _: IOException =>
+      recoveryStream
+    } >>> sink
+
+  val errorContained = failingStream.either >>> ZSink.collectAll
+
+  override def run = errorContained.debug.unit
